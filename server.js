18e7cd096e731886a8dec9ccc384541fdaa13e54
JebrilCzar <jgmacanas@up.edu.ph>
2025-03-05 23:44:53 +0800

    added GET methods

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import { appendFileSync } from "node:fs";
 import { readFileSync } from "node:fs";
+import { readFile } from "node:fs";
 
 // instantiate the server
 const app = express();
@@ -15,21 +16,40 @@ app.listen(3000, () => {
   console.log("Server started at port 3000");
 });
 
-app.get("/show-book", (req, res) => {
+app.get("/find-by-author", (req, res) => {
   var books = readFileSync("./books.txt", "utf-8");
   var books_instance = books.split("\n");
-  res.send(books_instance);
-  //   for (let i = 0; i < books_instance.length; i++) {
-  //     var attributes = books_instance[i].split(",");
-  //     if ((attributes[3] = "978-0-7475-3269-9")) {
-  //       res.send(books_instance[i]);
-  //     }
-  //   }
+  var results =[];
+  var results_index=0;
+  //length is reduced by 1 since the readfilesync captures one blank instance at the end
+  for (let i=0; i<books_instance.length-1; i++){
+    var attributes=books_instance[i].split(',');
+    //this is the index of author in the attributed array when they are separated by comma
+    if(attributes[2]==req.query.author){
+      results[results_index]=books_instance[i];
+      results_index++;
+    }
+  }
+  res.send(results);
 });
 
-// app.post("/submit-data", (req, res) => {
-//   res.send("Received a POST request" + req.body.name);
-// });
+app.get("/find-by-isbn-author", (req, res) => {
+  var books = readFileSync("./books.txt", "utf-8");
+  var books_instance = books.split("\n");
+  var results =[];
+  var results_index=0;
+  //length is reduced by 1 since the readfilesync captures one blank instance at the end
+  for (let i=0; i<books_instance.length-1; i++){
+    var attributes=books_instance[i].split(',');
+    //this is the index of author in the attributed array when they are separated by comma
+    if(attributes[2]==req.query.author & attributes[1]==req.query.isbn){
+      results[results_index]=books_instance[i];
+      results_index++;
+    }
+  }
+  res.send(results);
+});
+
 
 app.post("/add-book", (req, res) => {
   const success = { success: false };
