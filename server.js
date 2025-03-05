037e876689512ebfc9715534acb0a80b05e86819
JebrilCZar <jgmacanas@up.edu.ph>
2025-03-05 12:47:26 +0800

    added GET method

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import { appendFileSync } from "node:fs";
+import { readFileSync } from "node:fs";
 
 // instantiate the server
 const app = express();
@@ -14,9 +15,17 @@ app.listen(3000, () => {
   console.log("Server started at port 3000");
 });
 
-// app.get("/greeting", (req, res) => {
-//   res.send("Hello " + req.query.name + req.query.lmao);
-// });
+app.get("/show-book", (req, res) => {
+  var books = readFileSync("./books.txt", "utf-8");
+  var books_instance = books.split("\n");
+  res.send(books_instance);
+  //   for (let i = 0; i < books_instance.length; i++) {
+  //     var attributes = books_instance[i].split(",");
+  //     if ((attributes[3] = "978-0-7475-3269-9")) {
+  //       res.send(books_instance[i]);
+  //     }
+  //   }
+});
 
 // app.post("/submit-data", (req, res) => {
 //   res.send("Received a POST request" + req.body.name);
@@ -31,7 +40,17 @@ app.post("/add-book", (req, res) => {
     (req.body.Year != "")
   ) {
     try {
-      appendFileSync("books.txt", req.body.Book + "," + req.body.Author + "\n");
+      appendFileSync(
+        "books.txt",
+        req.body.Book +
+          "," +
+          req.body.ISBN +
+          "," +
+          req.body.Author +
+          "," +
+          req.body.Year +
+          "\n"
+      );
       success.success = true;
       res.send(success);
     } catch (err) {
