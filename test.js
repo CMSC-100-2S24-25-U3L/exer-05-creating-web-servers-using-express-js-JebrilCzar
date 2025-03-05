d6ccf6dcda9ea49927612a19a0c124571541f993
JebrilCZar <jgmacanas@up.edu.ph>
2025-03-05 12:47:02 +0800

    added more instances

--- test.js
+++ test.js
@@ -1,4 +1,5 @@
 import needle from "needle";
+import { readFileSync } from "node:fs";
 
 needle.post(
   "http://localhost:3000/add-book",
@@ -16,8 +17,8 @@ needle.post(
 needle.post(
   "http://localhost:3000/add-book",
   {
-    Book: "Harry Potter and the Chamber of Secrets",
-    ISBN: "",
+    Book: "Harry Potter and the Philosophers Stone",
+    ISBN: "978-0-7475-3269-9",
     Author: "J.K Rowling",
     Year: 1997,
   },
@@ -25,3 +26,28 @@ needle.post(
     console.log(res.body);
   }
 );
+
+needle.post(
+  "http://localhost:3000/add-book",
+  {
+    Book: "The Little Prince",
+    ISBN: "978-0156012195",
+    Author: "Antoine Saint-Exupery",
+    Year: 1943,
+  },
+  (err, res) => {
+    console.log(res.body);
+  }
+);
+
+// const books = readFileSync("./books.txt", "utf-8");
+// const books_instance = books.split("\n");
+
+// for (let i = 0; i < books_instance.length; i++) {
+//   var attributes = books_instance[i].split(",");
+//   if ((attributes[2] = "J.K Rowling")) {
+//     console.log(books_instance[i]);
+//   }
+// }
+//console.log(page.length);
+//console.log(page);
